6b8e3c86cc01b4c17e7ceb716538a1a993ef80d0
Wayd26 <wayedoun@gmail.com>
2019-09-07 19:49:31 +0100

    Merge branch 'master' of https://gitlab.com/projet-pta/new_frontend
    # Conflicts:
    #	src/app/dashboard/dashboard-routing.module.ts
    #	src/app/dashboard/dashboard.module.ts

---
+++ src/app/models/indicateur.model.ts
@@ -0,0 +1,40 @@
+import {Departement} from './departement.model';
+
+export interface Link {
+  rel: string;
+  href: string;
+}
+
+export interface Indicateur {
+  identifiant: number;
+  libelle: string;
+  valeur_cible: number;
+  valeur_realisee: number;
+  activite_id: string;
+  _user: string;
+  created_at: string;
+  updated_at: string;
+  links: Link[];
+}
+
+export interface Pagination {
+  total: number;
+  count: number;
+  per_page: number;
+  current_page: number;
+  total_pages: number;
+  links: any[];
+}
+
+export interface Meta {
+  pagination: Pagination;
+}
+
+export interface ListIndicateurResponse {
+  data: Indicateur[];
+  meta: Meta;
+}
+export interface IndicateurResponse {
+  data: Indicateur;
+}
+
